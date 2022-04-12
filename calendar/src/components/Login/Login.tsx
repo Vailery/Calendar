be35a96bc7702e054c06b7d0f242b751f789c6e6
Vailery <leraliabik8@gmail.com>
2022-04-12 17:29:00 +0300

    feat: add login page and button

---
+++ calendar/src/components/Login/Login.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useClient } from "../../services/ClientProvider";
+import { Button } from "../Button/Button";
+import styles from "./Login.module.css";
+
+declare global {
+  interface Window {
+    gapi: any;
+  }
+}
+
+export const Login = () => {
+  const client = useClient();
+  const history = useHistory();
+
+  const handleAuthClick = () => {
+    client.setClient(window.gapi);
+
+    client.client.auth2.getAuthInstance().signIn();
+  };
+
+  useEffect(() => {
+    if (client.isSignedIn && client) {
+      history.push("/home");
+    }
+  }, [client, client.isSignedIn]);
+
+  useEffect(() => {
+    client.setClient(window.gapi);
+  }, [client]);
+
+  return (
+    <div className={styles.main}>
+      <p className={styles.text}>Calendar</p>
+      <Button onClick={handleAuthClick} text="Login" />
+    </div>
+  );
+};
